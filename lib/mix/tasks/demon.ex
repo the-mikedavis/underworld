dad42749bc399ad6ae225150015da316ef9f141e
Michael Davis <mcd164@miami.edu>
2019-02-22 17:12:23 -0500

    Provide shells for the mix tasks

---
+++ lib/mix/tasks/demon.ex
@@ -0,0 +1,8 @@
+defmodule Mix.Tasks.Demon do
+  use Mix.Task
+
+  @shortdoc "Performs a static type analysis using the Dialyzer"
+  def run(_) do
+    :ok
+  end
+end
---
+++ lib/mix/tasks/spectre.ex
@@ -0,0 +1,8 @@
+defmodule Mix.Tasks.Spectre do
+  use Mix.Task
+
+  @shortdoc "Looks up specs for functions"
+  def run(_) do
+    :ok
+  end
+end
